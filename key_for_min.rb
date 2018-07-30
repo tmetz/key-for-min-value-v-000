3b9e93884e168aeb83e9f565992c73adbdd9d2ae
Tammy Metz <tammy@tammymetz.com>
2018-07-30 08:34:19 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil
   else
-    smallest = name_hash[0]
+    smallest = name_hash.default
     name_hash.each do |key, value|
       if value < smallest
         smallest = value
