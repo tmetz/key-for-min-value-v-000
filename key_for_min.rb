9b4e462556c568ec33af4f13b44fb3f50473939d
Tammy Metz <tammy@tammymetz.com>
2018-07-30 08:38:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
     return nil
   else
     smallest = nil
+    smallest_key = nil
     name_hash.each do |key, value|
       if smallest == nil
         smallest = value
